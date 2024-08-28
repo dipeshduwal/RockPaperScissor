6dbb67be2b21ed45e069e189ffc2707feb2830d6
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-08-28 11:33:42 +0545

    Function to get human choice and return it

--- javascript.js
+++ javascript.js
@@ -1,4 +1,23 @@
-function getComputerChoice(rock, paper, scissor){
-    return Math.floor(Math.random()*getComputerChoice.length);
+function getComputerChoice(){
+    const computerChoices = ["rock", "paper", "scissor"];
+    let randomizer = Math.floor(Math.random()*computerChoices.length);
+
+    return computerChoices[randomizer];
 }
 
+function getHumanChoice(){
+    let choice = prompt("What is your choice?").toLowerCase();
+
+    if (choice ==="rock"){
+        return choice
+    }
+
+    else if (choice === "paper"){
+        return choice
+    }
+
+    else if (choice === "scissor"){
+        return choice
+    }
+
+}
