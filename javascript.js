52f251085b5675c4290f63fdd07a3c78cb26c518
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-08-28 12:13:08 +0545

    Write Logic to play single round of rock,paper,scissor Use Switch case to determine the winner

--- javascript.js
+++ javascript.js
@@ -29,3 +29,52 @@ let humanScore = 0;
 let computerScore = 0;
 
 //Write the logic to play a single round
+function playRound(humanChoice, computerChoice){
+    switch (humanChoice){
+        case 'rock':
+            switch (computerChoice){
+                case 'rock':
+                    alert("It is a tie");
+                    break;
+                case 'scissor':
+                    alert("Congratulations, You win!");
+                    break;
+                case 'paper':
+                    alert("Sorry. You Lose.");
+                    break;
+            }
+        break;
+
+        case 'scissor':
+            switch (computerChoice){
+                case 'rock':
+                    alert("Sorry. You Lose.");
+                    break;
+                case 'scissor':
+                    alert("It is a tie");
+                    break;
+                case 'paper':
+                    alert("Congratulations, You win!");
+                    break;
+            }
+        break;
+
+        case 'paper':
+            switch (computerChoice){
+                case 'rock':
+                    alert("Congratulations, You win!");
+                    break;
+                case 'scissor':
+                    alert("Sorry. You lose.");
+                    break;
+                case 'paper':
+                    alert("It is a tie");
+                    break;
+            }
+        break;
+
+        default:
+            alert("Invalid Input. Please play rock, paper, scissor.");
+    }
+}
+
